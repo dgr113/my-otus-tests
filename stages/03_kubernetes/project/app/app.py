7b50f8212705311a190410a9ce5287690d043575
d.grigoriev <dmitry-gr87@yandex.ru>
2020-11-06 23:30:15 +0300

    Update commit 3

--- stages/03_kubernetes/project/app/app.py
+++ stages/03_kubernetes/project/app/app.py
@@ -1,25 +1,42 @@
 # coding: utf-8
 
 import os
-from flask import Flask, jsonify
+import json
+from flask import Flask
+from sqlalchemy import create_engine  # type: ignore
 
 app = Flask(__name__)
 
+config = {
+    'DATABASE_URI': os.environ.get('DATABASE_URI', ''),
+    'HOSTNAME': os.environ['HOSTNAME'],
+    'GREETING': os.environ.get('GREETING', 'Hello'),
+}
+
+engine = create_engine(config['DATABASE_URI'], echo=True)
 
 
 
-@app.route('/')
-def index():
-    return "Hello from: {}".format( os.environ['HOSTNAME'] )
+@app.route("/")
+def hello():
+    return config['GREETING'] + ' from ' + config['HOSTNAME'] + '!'
 
 
+@app.route("/config")
+def configuration():
+    # config['version'] = 'NEW'
+    return json.dumps(config)
 
-@app.route('/health')
-def health():
-    return jsonify({'Status': 'OK'})
 
+@app.route('/db')
+def db():
+    rows = []
+    with engine.connect() as connection:
+        result = connection.execute("select id, name from client;")
+        rows = [ dict(r.items()) for r in result ]
+    return json.dumps(rows)
 
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='80')
+    app.run(host='0.0.0.0', port='80', debug=True)
