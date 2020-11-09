6fdba39522471abebb91c9047128a8a370088b1e
d.grigoriev <dmitry-gr87@yandex.ru>
2020-11-09 23:56:16 +0300

    Update commit 12

--- stages/03_kubernetes/project/app/app.py
+++ stages/03_kubernetes/project/app/app.py
@@ -2,10 +2,15 @@
 
 import os
 import json
+from ast import literal_eval
+
 from flask import Flask, jsonify
+from flask_restful import Api, Resource  # type: ignore
 from sqlalchemy import create_engine  # type: ignore
 
+
 app = Flask(__name__)
+api = Api(app)
 
 app_config = {
     'DATABASE_URI': os.environ.get('DATABASE_URI', ''),
@@ -18,25 +23,47 @@ engine = create_engine(app_config['DATABASE_URI'], echo=True)
 
 
 
-@app.route("/")
-def index():
-    return "{} from {}!".format(app_config['GREETING'], app_config['HOSTNAME'])
+class Index(Resource):
+    def get(self):
+        return "{} from {}!".format(app_config['GREETING'], app_config['HOSTNAME'])
 
 
-@app.route("/config")
-def config():
-    return jsonify(app_config)
+class Config(Resource):
+    def get(self):
+        return jsonify(app_config)
 
 
-@app.route('/db')
-def db():
-    with engine.connect() as connection:
-        result = connection.execute("select id, name from client;")
-        rows = [ dict(r.items()) for r in result ]
-    return json.dumps(rows)
+class StudentList(Resource):
+    def get(self) -> str:
+        with engine.connect() as connection:
+            sql_expr = "SELECT id, name FROM client;"
+            result = connection.execute(sql_expr)
+            rows = [ dict(r.items()) for r in result ]
+        return json.dumps(rows)
+
+
+class Student(Resource):
+    def get(self, student_id: str) -> str:
+        with engine.connect() as connection:
+            rec_id = literal_eval(student_id)
+            sql_expr = "SELECT id, name FROM client WHERE id = {};".format( rec_id )
+            result = connection.execute(sql_expr)
+            rows = [ dict(r.items()) for r in result ]
+        return json.dumps(rows)
+
+    def delete(self, student_id: str) -> str:
+        with engine.connect() as connection:
+            rec_id = literal_eval(student_id)
+            sql_expr = "DELETE FROM client WHERE id = {};".format( rec_id )
+            connection.execute(sql_expr)
+        return json.dumps({'status': 200})
 
 
 
 
 if __name__ == "__main__":
+    api.add_resource(Index, '/')
+    api.add_resource(Config, '/config')
+    api.add_resource(StudentList, '/student')
+    api.add_resource(Student, '/student/<string:student_id>')
     app.run(host='0.0.0.0', port='80', debug=True)
