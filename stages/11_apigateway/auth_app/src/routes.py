a3acc1a7dc2149b69e47e89c50c6500798bdbf1d
d.grigoriev <dmitry-gr87@yandex.ru>
2021-02-16 01:58:07 +0300

    Update commit 18

--- stages/11_apigateway/auth_app/src/routes.py
+++ stages/11_apigateway/auth_app/src/routes.py
@@ -1,29 +1,11 @@
 # coding: utf-8
 
-from typing import Sequence, Optional, Callable
+from typing import Sequence, Optional, Callable, Any
 from flask import jsonify, current_app, request, abort
 from flask_restful import Resource  # type: ignore
 
 from .models import Users  # type: ignore
 from .jwt import JwtUtils
-
-
-
-
-def check_auth(func: Callable) -> Callable:
-    def wrapped(_self, user_id: Optional[int], *args, **kwargs):
-        signed_user_data = {
-            'id': request.headers.get('X-User-Id', None),
-            'name': request.headers.get('X-User-Name', None)
-        }
-        signer_user_id = signed_user_data['id']
-        if ( user_id is not None ) or ( user_id == signer_user_id ):
-            return func(signer_user_id, *args, **kwargs)
-        else:
-            # Logging an unauthorized access attempt
-            abort(403, "Wrong user auth data")
-
-    return wrapped
 
 
 
@@ -64,16 +46,37 @@ class UserRegister( Resource ):
 
 
 
-class UserActions(Resource):
-    @check_auth
+class UserActions( Resource ):
+    class Inner:
+        @staticmethod
+        def check_auth(func: Callable[..., Any]) -> Callable[..., Any]:
+            """ Wrapper for verifying and passing trusted user data into wrapped function:
+                1. Doesn't allow the absence of trusted headers (user_id)
+                2. Compares user data and trusted data
+                3. If there is no user data, passes trusted data into wrapped function
+            """
+            def wrapped(self, user_id: Optional[int], *args, **kwargs) -> Any:
+                trusted_user_data = {
+                    'id': request.headers.get('X-User-Id', None),
+                    'name': request.headers.get('X-User-Name', None)
+                }
+                if ( trusted_user_id := trusted_user_data['id'] ) and ( user_id is None or user_id == trusted_user_id ):
+                    return func(self, trusted_user_id, *args, **kwargs)
+                else:
+                    # Logging an unauthorized access attempt
+                    abort(403, "Wrong user auth data")
+
+            return wrapped
+
+    @Inner.check_auth
     def get(self, user_id: Optional[int] = None) -> Sequence[dict]:
         return Users.get_one( user_id )
 
-    @check_auth
+    @Inner.check_auth
     def put(self, user_id: Optional[int] = None) -> str:
         req_data = request.get_json()
         return Users.update(user_id, req_data)
 
-    @check_auth
+    @Inner.check_auth
     def delete(self, user_id: Optional[int] = None) -> int:
-        return Users.delete(user_id)
+        return Users.delete( user_id )
